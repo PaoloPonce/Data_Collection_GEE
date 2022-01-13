eb10e4c6ab60f84fe4d9f06746b41eee76da4322
Paolo Ponce Aranguri <50393597+PaoloPonce@users.noreply.github.com>
2022-01-13 17:24:03 -0500

    Se inicia el proyecto

---
+++ stats_clima.js
@@ -0,0 +1,42 @@
+// Import shape files for distritos and peru
+var table = ee.FeatureCollection("users/a20164808/SHAPE_DISTRITOS"),
+    region = ee.FeatureCollection("users/a20164808/FRONT_PERU");
+
+// Flatten feature collection from distritos	
+var distris = ee.FeatureCollection([table]).flatten();
+
+// Import clima image collection, clip for peru and select bands
+var modisNDVI = ee.ImageCollection("IDAHO_EPSCOR/TERRACLIMATE")
+ .map(clipToCol)
+ .select(["tmmn","tmmx","pr"]);
+  
+function clipToCol(image) { 
+  return image.clip(region); 
+}
+
+// Set start and end time
+var startDate = ee.Date('1990-01-01');
+var endDate = ee.Date('2020-12-01');
+
+// Filter by dates
+var modisNDVI = modisNDVI.filterDate(startDate,endDate)
+
+// Custom function
+var getMeanVals = function(image){
+  image = ee.Image(image)
+  var date = ee.Date(image.get('system:time_start')).format('YYYY-MM-dd')
+  var mean = image.reduceRegions({
+      collection: distris,
+      reducer: ee.Reducer.mean(),
+      scale: 4638 
+    })
+  return mean
+    .map(function(f){
+      return f.set({'date': date})
+    })
+}
+
+// Apply custom function
+var means = modisNDVI
+  .map(getMeanVals)
+  .flatten()
